508af4b981fcd6873597ade47f07264d333c7571
Paul Moss <moss.paul@gmail.com>
2016-11-24 13:26:53 +0000

    basic user profile page

--- freelancefinder/users/views.py
+++ freelancefinder/users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.base import TemplateView
+
+
+class UserProfileView(TemplateView):
+
+    template_name = "users/profile.html"
