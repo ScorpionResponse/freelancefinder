105ccba1496587ccea6b0df65277291825a11367
Paul Moss <moss.paul@gmail.com>
2016-11-24 13:48:55 +0000

    Static file handling

--- freelancefinder/freelancefinder/urls.py
+++ freelancefinder/freelancefinder/urls.py
@@ -1,12 +1,12 @@
-from django.conf import settings
 from django.conf.urls import url, include
-from django.conf.urls.static import static
 from django.contrib import admin
 
 from .views import IndexPageView
 
+admin.autodiscover()
+
 urlpatterns = [
     url(r'^$', IndexPageView.as_view(), name='index'),
-    url(r'^user/', include('users.urls')),
+    url(r'^users/', include('users.urls')),
     url(r'^freelance_admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
