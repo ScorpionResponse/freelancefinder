032701c192952f3152ff4642930706582f46dd8c
Paul Moss <moss.paul@gmail.com>
2016-12-06 16:25:53 +0000

    add post title, fix path to static

--- freelancefinder/jobs/models.py
+++ freelancefinder/jobs/models.py
@@ -9,4 +9,5 @@ class Post(models.Model):
     url = models.URLField()
     source = models.ForeignKey('remotes.Source', on_delete=models.SET_NULL, blank=True, null=True, related_name="posts")
     date_added = models.DateTimeField(default=datetime.datetime.now)
+    title = models.CharField(max_length=255)
     description = models.TextField()
