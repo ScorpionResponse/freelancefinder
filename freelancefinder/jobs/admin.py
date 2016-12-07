56559d98fc827a82a825db8cf46b85d2982136d4
Paul Moss <moss.paul@gmail.com>
2016-12-07 19:22:07 +0000

    add job admin

--- freelancefinder/jobs/admin.py
+++ freelancefinder/jobs/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Job
+
+
+class JobAdmin(admin.ModelAdmin):
+    pass
 
 
 class PostAdmin(admin.ModelAdmin):
     pass
 
+
+admin.site.register(Job, JobAdmin)
 admin.site.register(Post, PostAdmin)
