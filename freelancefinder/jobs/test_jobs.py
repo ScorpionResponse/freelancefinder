84317446b67f056a4c4ebbf59300c052e99c429b
Paul Moss <moss.paul@gmail.com>
2016-12-09 15:46:05 +0000

    test all templates

--- freelancefinder/jobs/test_jobs.py
+++ freelancefinder/jobs/test_jobs.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
 
-# Create your tests here.
+def test_job_list(client):
+    response = client.get('/jobs/job-list/')
+    assert response.status_code == 200
+
+
+def test_post_list(client):
+    response = client.get('/jobs/post-list/')
+    assert response.status_code == 200
--- freelancefinder/remotes/test_remotes.py
+++ freelancefinder/remotes/test_remotes.py
@@ -1,3 +1,4 @@
-from django.test import TestCase
 
-# Create your tests here.
+def test_source_list(client):
+    response = client.get('/remotes/source-list/')
+    assert response.status_code == 200
