aff2e244c9e3fa2e9034ae452159af349be95ca1
Paul Moss <moss.paul@gmail.com>
2016-12-09 15:00:38 +0000

    just remove skipped test

--- freelancefinder/users/test_users.py
+++ freelancefinder/users/test_users.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 
 from authtools.models import User
-import pytest
 
 
 class LoginTests(TestCase):
@@ -24,20 +23,6 @@ class LoginTests(TestCase):
         """Test that logging in with an incorrect password fails"""
         self.client.login(username='test1@example.com', password='fail')
         response = self.client.get('/')
-        assert not str(response.context['user']).endswith('<test1@example.com>')
-        self.assertEqual(str(response.context['user']), 'AnonymousUser',
-                         "Since the login failed, should still be anonymous, but are not")
-
-    @pytest.mark.skip(reason="login form not created yet")
-    def test_failed_login_in_form(self):
-        """
-        Test that logging in with an incorrect password generates an error on
-        the page
-        """
-        data = {'username': 'test1', 'password': 'fail'}
-        response = self.client.post('/accounts/login', data)
-        self.assertFormError(response, 'form', None,
-                             u'Please enter a correct username and password. Note that both fields may be case-sensitive.')
         assert not str(response.context['user']).endswith('<test1@example.com>')
         self.assertEqual(str(response.context['user']), 'AnonymousUser',
                          "Since the login failed, should still be anonymous, but are not")
