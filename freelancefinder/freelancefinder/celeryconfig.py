17ae48a2cb69638b1db9ecfb46ac3e264fc31dca
Paul Moss <moss.paul@gmail.com>
2016-12-11 12:22:27 +0000

    Just don't have stupid naming conflicts

--- freelancefinder/freelancefinder/celeryconfig.py
+++ freelancefinder/freelancefinder/celeryconfig.py
@@ -5,8 +5,8 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freelancefinder.settings')
 
-app = Celery('freelancefinder')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+celery_app = Celery('freelancefinder')
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load tasks.py modules from all registered Django app configs.
-app.autodiscover_tasks()
+celery_app.autodiscover_tasks()
