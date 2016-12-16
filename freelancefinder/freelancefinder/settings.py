a6ea327e98340ada26fa030206f39ab123259ea5
Paul Moss <moss.paul@gmail.com>
2016-12-16 11:51:03 +0000

    logging config

--- freelancefinder/freelancefinder/settings.py
+++ freelancefinder/freelancefinder/settings.py
@@ -83,6 +83,113 @@ DATABASES = {
     'default': env.db()
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue'
+        },
+        'request_id': {
+            '()': 'log_request_id.filters.RequestIDFilter'
+        },
+    },
+    'formatters': {
+        'main': {
+            '()': 'pythonjsonlogger.jsonlogger.JsonFormatter',
+            # Since we're doing json logging, just throw everything in there
+            'format': '''
+                %(threadName)s %(name)s %(thread)d %(created)f
+                %(process)d %(processName)s %(relativeCreated)f
+                %(module)s %(funcName)s %(levelno)d %(msecs)f
+                %(pathname)s %(lineno)d %(asctime)s %(message)s
+                %(filename)s %(levelname)s %(request_id)s
+            ''',
+            'datefmt': "%Y-%m-%d %H:%M:%S",
+        },
+        'celery': {
+            '()': 'pythonjsonlogger.jsonlogger.JsonFormatter',
+            # Since we're doing json logging, just throw everything in there
+            'format': '''
+                %(threadName)s %(name)s %(thread)d %(created)f
+                %(process)d %(processName)s %(relativeCreated)f
+                %(module)s %(funcName)s %(levelno)d %(msecs)f
+                %(pathname)s %(lineno)d %(asctime)s %(message)s
+                %(filename)s %(levelname)s
+            ''',
+            'datefmt': "%Y-%m-%d %H:%M:%S",
+        },
+    },
+    'handlers': {
+        'email': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'filters': ['require_debug_false', 'request_id'],
+        },
+        'logfile': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': '/var/log/django/freelancefinder.log',
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 7,
+            'formatter': 'main',
+            'filters': ['require_debug_false', 'request_id'],
+        },
+        'celery': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': '/var/log/django/freelancefinder_celery.log',
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 7,
+            'formatter': 'celery',
+            'filters': ['require_debug_false'],
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'main',
+            'filters': ['require_debug_true', 'request_id'],
+        },
+        'debug': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'django_debug.log',
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 7,
+            'formatter': 'main',
+            'filters': ['require_debug_true', 'request_id'],
+        },
+        'null': {
+            "class": 'logging.NullHandler',
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['email', 'console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'django': {
+            'handlers': ['null', ],
+        },
+        'celery': {
+            'handlers': ['console', 'celery', 'debug'],
+            'level': 'DEBUG',
+        },
+        'py.warnings': {
+            'handlers': ['null', ],
+        },
+        '': {
+            'handlers': ['console', 'logfile', 'debug'],
+            'level': "DEBUG",
+        },
+    }
+}
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
