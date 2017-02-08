b81b486781fe5cbfcf504a764347a80cd93c2281
Paul Moss <moss.paul@gmail.com>
2017-02-08 20:49:13 +0000

    try to add scheduled task

--- freelancefinder/freelancefinder/celeryconfig.py
+++ freelancefinder/freelancefinder/celeryconfig.py
@@ -2,6 +2,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freelancefinder.settings')
 
@@ -10,3 +11,11 @@ celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load tasks.py modules from all registered Django app configs.
 celery_app.autodiscover_tasks()
+
+# Configure periodic tasks
+celery_app.conf.beat_schedule = {
+    'harvest_every_minute': {
+        'task': 'remotes.tasks.harvest_sources',
+        'schedule': crontab(),
+    },
+}
---
+++ freelancefinder/remotes/tasks.py
@@ -0,0 +1,18 @@
+"""Tasks to perform periodic tasks with remotes."""
+
+from celery.utils.log import get_task_logger
+
+from freelancefinder import celery_app
+from .models import Source
+
+logger = get_task_logger(__name__)
+
+
+@celery_app.task
+def harvest_sources():
+    """Get a new batch of data from all sources."""
+    for source in Source.objects.all():
+        logger.info("Harvesting from Source: %s", source)
+        harvester = source.harvester()
+        for post in harvester.harvest():
+            logger.info("Got new Post: %s", post)
