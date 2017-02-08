a6e319326d365b5f218ee07ef74e87bb4e064d1d
Paul Moss <moss.paul@gmail.com>
2017-02-08 18:15:02 +0000

    pylint fixes

--- freelancefinder/remotes/sources/reddit/harvest.py
+++ freelancefinder/remotes/sources/reddit/harvest.py
@@ -6,13 +6,18 @@ from jobs.models import Post
 class Harvester(object):
     """Simple Harvester to gather reddit posts."""
 
-    def __init__(self, code, name):
+    def __init__(self, source):
         """Init the harvester with basic info."""
-        self.code = code
-        self.name = name
+        self.source = source
+        self.status_info = {'count': 0}
 
     def harvest(self):
         """Gather some Posts from reddit."""
         for index in range(10):
-            post = Post(url="http://reddit.com/", title="Test Post {}".format(index), description="Steve")
+            post = Post(url="http://reddit.com/", source=self.source, title="Test Post {}".format(index), description="Steve")
+            self.status_info['count'] += 1
             yield post
+
+    def status(self):
+        """Return the current status of this harvester."""
+        return self.status_info
