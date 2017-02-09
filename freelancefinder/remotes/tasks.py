906b9e7143e8a4940790d427ca386d5f60bde65d
Paul Moss <moss.paul@gmail.com>
2017-02-09 21:43:48 +0000

    don't do module level imports

--- freelancefinder/remotes/tasks.py
+++ freelancefinder/remotes/tasks.py
@@ -3,14 +3,13 @@
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
-from .models import Source
-
 logger = get_task_logger(__name__)
 
 
 @shared_task
 def harvest_sources():
     """Get a new batch of data from all sources."""
+    from .models import Source
     for source in Source.objects.all():
         logger.info("Harvesting from Source: %s", source)
         harvester = source.harvester()
