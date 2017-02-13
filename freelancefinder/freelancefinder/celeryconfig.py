254d2dfc0c6574d41d0a28c66d4157de7aa57272
Paul Moss <moss.paul@gmail.com>
2017-02-13 17:54:13 +0000

    try alternate configuration method

--- freelancefinder/freelancefinder/celeryconfig.py
+++ freelancefinder/freelancefinder/celeryconfig.py
@@ -2,7 +2,9 @@
 import os
 
 from celery import Celery
-from celery.schedules import crontab
+
+from django_celery_beat.models import IntervalSchedule, PeriodicTask
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freelancefinder.settings')
 
@@ -12,10 +14,12 @@ celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load tasks.py modules from all registered Django app configs.
 celery_app.autodiscover_tasks()
 
-# Configure periodic tasks
-celery_app.conf.beat_schedule = {
-    'harvest_every_10_minutes': {
-        'task': 'remotes.tasks.harvest_sources',
-        'schedule': crontab(minute='*/10'),
-    },
-}
+
+@celery_app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    """Ensure periodic tasks are present in the DB."""
+
+    schedule, created = IntervalSchedule.objects.get_or_create(every=10, period=IntervalSchedule.minutes)
+
+    PeriodicTask.get_or_create(interval=schedule, name='Harvest Remotes',
+                               task='remotes.tasks.harvest_sources')
