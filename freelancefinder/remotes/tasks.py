50b40adf65223b425fa5644a3b824d1728ab2619
Paul Moss <moss.paul@gmail.com>
2017-02-21 12:20:00 +0000

    logger calls

--- freelancefinder/remotes/tasks.py
+++ freelancefinder/remotes/tasks.py
@@ -15,11 +15,15 @@ logger = get_task_logger(__name__)
 def setup_periodic_tasks(sender, **kwargs):
     """Ensure periodic tasks are present in the DB."""
 
+    logger.info("Configuring periodic tasks.")
+
     schedule, created = IntervalSchedule.objects.get_or_create(every=10, period=IntervalSchedule.MINUTES)
 
     pertask, created = PeriodicTask.objects.get_or_create(interval=schedule, name='Harvest Remotes', task='remotes.tasks.harvest_sources', expires=maya.now().add(minutes=30).datetime())
+    logger.debug("Got periodic task: %s", pertask)
 
     if not created and pertask.expires < maya.now().datetime():
+        logger.info("Deleting old periodic task and creating a new one.")
         pertask.delete()
         PeriodicTask.objects.get_or_create(interval=schedule, name='Harvest Remotes', task='remotes.tasks.harvest_sources', expires=maya.now().add(minutes=30).datetime())
 
