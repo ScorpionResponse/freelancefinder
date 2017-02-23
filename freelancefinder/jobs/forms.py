3f3e9e06b52aaf045f8287397d72be1a016df62e
Paul Moss <moss.paul@gmail.com>
2017-02-23 13:44:18 +0000

    call form properly

--- freelancefinder/jobs/forms.py
+++ freelancefinder/jobs/forms.py
@@ -6,6 +6,6 @@ from django import forms
 class PostFilterForm(forms.Form):
     """Form for filtering the PostListView."""
 
-    title = forms.CharField()
-    is_job_posting = forms.BooleanField()
-    is_freelance = forms.BooleanField()
+    title = forms.CharField(required=False)
+    is_job_posting = forms.BooleanField(required=False)
+    is_freelance = forms.BooleanField(required=False)
