66b48471ae0201e796b69f08ae4653f32dddd2e6
Paul Moss <moss.paul@gmail.com>
2017-03-01 16:52:53 +0000

    organize the task scheduling

--- freelancefinder/jobs/tasks.py
+++ freelancefinder/jobs/tasks.py
@@ -1,6 +1,5 @@
 """Tasks to handle periodic tasks for jobs."""
 
-from django_celery_beat.models import IntervalSchedule, PeriodicTask
 from celery import Celery
 from celery.utils.log import get_task_logger
 
@@ -9,27 +8,13 @@ celery_app = Celery()
 logger = get_task_logger(__name__)
 
 
-@celery_app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    """Ensure periodic tasks are scheduled."""
-    logger.info("Configuring periodic tasks.")
-    logger.debug('Sender: %s; kwargs: %s', sender, kwargs)
-
-    schedule, created = IntervalSchedule.objects.get_or_create(every=4, period=IntervalSchedule.MINUTES)
-    logger.debug("IntervalSchedule: %s; Created: %s", schedule, created)
-
-    pertask, created = PeriodicTask.objects.get_or_create(interval=schedule, name="Process New Posts", task='jobs.tasks.process_new_posts')
-    logger.debug("PeriodicTask: %s; Created: %s", pertask, created)
-    pertask, created = PeriodicTask.objects.get_or_create(interval=schedule, name="Create Jobs from Posts", task='jobs.tasks.create_jobs')
-    logger.debug("PeriodicTask: %s; Created: %s", pertask, created)
-
-
 @celery_app.task
 def process_new_posts():
     """Determine which posts are about freelance jobs."""
     from .models import Post
 
     for post in Post.objects.new():
+        logger.debug("Processing post: %s", post)
         post.processed = True
         if '[hiring]' in post.title.lower():
             post.is_job_posting = True
@@ -43,6 +28,7 @@ def create_jobs():
     from .models import Post, Job
 
     for post in Post.objects.pending_freelance_jobs():
+        logger.debug("Creating job from post: %s", post)
         job = Job.objects.create(title=post.title, description=post.description)
         post.job = job
         post.save()
--- freelancefinder/remotes/tasks.py
+++ freelancefinder/remotes/tasks.py
@@ -1,35 +1,11 @@
 """Tasks to perform periodic tasks with remotes."""
 
-from django_celery_beat.models import IntervalSchedule, PeriodicTask
 from celery import Celery
 from celery.utils.log import get_task_logger
-
-import maya
 
 
 celery_app = Celery()
 logger = get_task_logger(__name__)
-
-
-@celery_app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    """Ensure periodic tasks are present in the DB."""
-    logger.info("Configuring periodic tasks.")
-    logger.debug('Sender: %s; kwargs: %s', sender, kwargs)
-
-    schedule, created = IntervalSchedule.objects.get_or_create(every=10, period=IntervalSchedule.MINUTES)
-
-    pertask, created = PeriodicTask.objects.get_or_create(interval=schedule, name='Harvest Remotes', task='remotes.tasks.harvest_sources')
-    logger.debug("Got periodic task: %s", pertask)
-
-    if created:
-        logger.debug('Newly created, set expiry.')
-        pertask.expires = maya.now().add(minutes=30).datetime()
-        pertask.save()
-    else:
-        logger.info("Deleting old periodic task and creating a new one.")
-        pertask.delete()
-        PeriodicTask.objects.create(interval=schedule, name='Harvest Remotes', task='remotes.tasks.harvest_sources', expires=maya.now().add(minutes=30).datetime())
 
 
 @celery_app.task
