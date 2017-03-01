f70d60dba34b867ecffab2308ca29c2f705fd406
Paul Moss <moss.paul@gmail.com>
2017-03-01 13:30:01 +0000

    add docstrings

--- freelancefinder/jobs/views.py
+++ freelancefinder/jobs/views.py
@@ -22,9 +22,11 @@ class PostListView(FormMixin, ListView):
     template_name = "jobs/post_list.html"
 
     def get_queryset(self):
+        """Queryset should sort by desc created by default."""
         return Post.objects.all().order_by('-created')
 
     def get_context_data(self, **kwargs):
+        """Include search/filter form."""
         context = super(PostListView, self).get_context_data(**kwargs)
 
         context['form'] = self.get_form()
