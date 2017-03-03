2342985b4399e87ad382f0d10b69c95f646b6e1c
Paul Moss <moss.paul@gmail.com>
2017-03-03 16:10:14 +0000

    sanitize html

--- freelancefinder/jobs/templatetags/markdown.py
+++ freelancefinder/jobs/templatetags/markdown.py
@@ -3,13 +3,14 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 
+import bleach
 import markdown
 
 register = template.Library()
 
 
-@register.filter(name='markdown', is_safe=True)
+@register.filter(is_safe=True, name='markdown')
 @stringfilter
 def markdown_filter(value):
     """Convert markdown value to html."""
-    return markdown.markdown(value)
+    return bleach.clean(markdown.markdown(value))
