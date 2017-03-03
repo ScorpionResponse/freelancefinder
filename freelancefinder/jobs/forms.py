f0846135fd16e3c9650a4d726769ae5f72ed2909
Paul Moss <moss.paul@gmail.com>
2017-03-03 17:31:40 +0000

    make form inline

--- freelancefinder/jobs/forms.py
+++ freelancefinder/jobs/forms.py
@@ -2,6 +2,9 @@
 
 from django import forms
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
 
 class PostFilterForm(forms.Form):
     """Form for filtering the PostListView."""
@@ -9,3 +12,10 @@ class PostFilterForm(forms.Form):
     title = forms.CharField(required=False)
     is_job_posting = forms.BooleanField(required=False)
     is_freelance = forms.BooleanField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(PostFilterForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-inline'
+        self.helper.form_method = 'get'
+        self.helper.add_input(Submit('submit', 'Filter'))
