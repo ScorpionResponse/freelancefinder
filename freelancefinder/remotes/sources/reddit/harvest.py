f465c4367b033a00a92f3e64fef494f11f3adcd7
Paul Moss <moss.paul@gmail.com>
2017-03-07 16:36:39 +0000

    Harvest per subreddit

--- freelancefinder/remotes/sources/reddit/harvest.py
+++ freelancefinder/remotes/sources/reddit/harvest.py
@@ -1,6 +1,8 @@
 """Harvest process for the Reddit Source."""
 
+from collections import defaultdict
 import logging
+
 import praw
 
 from django.conf import settings
@@ -16,7 +18,7 @@ class Harvester(object):
     def __init__(self, source):
         """Init the harvester with basic info."""
         self.source = source
-        self.status_info = {'count': 0}
+        self.status_info = defaultdict(int)
         self.client = praw.Reddit(client_id=settings.REDDIT_CLIENT_ID,
                                   client_secret=settings.REDDIT_CLIENT_SECRET,
                                   user_agent=settings.REDDIT_USER_AGENT)
@@ -24,16 +26,17 @@ class Harvester(object):
     def harvest(self):
         """Gather some Posts from reddit."""
         subreddits_to_monitor = self.source.config.filter(config_key='subreddits').first().config_value.split('|')
-        subreddit_joined = '+'.join(subreddits_to_monitor)
-        for submission in self.client.subreddit(subreddit_joined).new(limit=100):
-            if Post.objects.filter(source=self.source, unique=submission.id).exists():
-                logger.info("Reddit harvester got duplication post id %s, assuming everything new is harvested.", submission.id)
-                break
-            else:
-                post = Post(url=submission.url, source=self.source, title=submission.title[:255], description=submission.selftext, unique=submission.id, subarea=submission.subreddit)
-                self.status_info['count'] += 1
-                yield post
-        logger.info("Reddit harvester got %s posts", self.status_info['count'])
+        for subr in subreddits_to_monitor:
+            for submission in self.client.subreddit(subr).new(limit=100):
+                if Post.objects.filter(source=self.source, unique=submission.id).exists():
+                    logger.info("Reddit harvester got duplication post id %s in subreddit %s, assuming everything new is harvested.", submission.id, subr)
+                    break
+                else:
+                    post = Post(url=submission.url, source=self.source, title=submission.title[:255], description=submission.selftext, unique=submission.id, subarea=submission.subreddit)
+                    self.status_info['count-%s' % (subr,)] += 1
+                    self.status_info['total'] += 1
+                    yield post
+        logger.info("Reddit harvester status: %s", dict(self.status_info))
 
     def status(self):
         """Return the current status of this harvester."""
