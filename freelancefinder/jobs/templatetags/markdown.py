91fabcde2eaf3c47146c3d5ab263f674978ed66e
Paul Moss <moss.paul@gmail.com>
2017-03-09 18:14:31 +0000

    change markdown import name

--- freelancefinder/jobs/templatetags/markdown.py
+++ freelancefinder/jobs/templatetags/markdown.py
@@ -4,7 +4,7 @@ from django import template
 from django.utils.safestring import mark_safe
 
 import bleach
-import markdown
+import markdown as markdown_
 
 register = template.Library()
 
@@ -14,6 +14,6 @@ ADDITIONAL_TAGS = ['p', 'br']
 @register.filter(name='markdown')
 def markdown_filter(value):
     """Convert markdown value to html."""
-    rendered_html = markdown.markdown(value)
+    rendered_html = markdown_.markdown(value)
     bleached_html = bleach.clean(rendered_html, tags=bleach.ALLOWED_TAGS + ADDITIONAL_TAGS, strip=True)
     return mark_safe(bleached_html)
