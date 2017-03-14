9339cfd718bbd99835811a51a9baed997a889a14
Paul Moss <moss.paul@gmail.com>
2017-03-14 21:46:07 +0000

    add pagination helper

---
+++ freelancefinder/jobs/templatetags/within_page_range.py
@@ -0,0 +1,13 @@
+"""within_page_range template filter."""
+
+import math
+
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='within_page_range')
+def within_filter(test_page, current_page):
+    """Check whether test_page is within 5 of current_page."""
+    return math.fabs(current_page - test_page) <= 10
