3b447ed0e2a9d1803ce4c45a67371fadb47a9a2a
Paul Moss <moss.paul@gmail.com>
2017-03-15 16:26:12 +0000

    change note

--- freelancefinder/jobs/templatetags/within_page_range.py
+++ freelancefinder/jobs/templatetags/within_page_range.py
@@ -9,5 +9,5 @@ register = template.Library()
 
 @register.filter(name='within_page_range')
 def within_filter(test_page, current_page):
-    """Check whether test_page is within 5 of current_page."""
+    """Check whether test_page is within 10 of current_page."""
     return math.fabs(current_page - test_page) <= 10
