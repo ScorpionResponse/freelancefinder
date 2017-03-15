84fcb1f7c3420ae992949cf271a287c8a3279227
Paul Moss <moss.paul@gmail.com>
2017-03-15 23:16:18 +0000

    show username

--- freelancefinder/users/utils.py
+++ freelancefinder/users/utils.py
@@ -1,6 +1,10 @@
 """User related utilities."""
 
+import logging
+
 from django.core.cache import cache
+
+logger = logging.getLogger(__name__)
 
 
 def is_in_group(user, groups):
@@ -8,11 +12,13 @@ def is_in_group(user, groups):
     Returns a boolean indicating if a user belongs to a group.
     Usage: {% if request.user|in_group:"Group Name" %}
     """
+    logger.debug('Checking whether user (%s) is in groups (%s)', user, groups)
     groups = groups.split('|')
     groups = [g.strip() for g in groups]
     cache_key = 'in_group_{}_{}'.format(user, ''.join(groups).replace(' ', ''))
 
     user_is_in_group = cache.get(cache_key)
+    logger.debug('Cache key %s result: %s', cache_key, user_is_in_group)
     if user_is_in_group is None:
         try:
             user_is_in_group = user.groups.filter(name__in=groups).exists()
