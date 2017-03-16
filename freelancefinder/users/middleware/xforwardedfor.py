77aba029da6f9d3bf0f88cc7b6ce6c779a9d8eef
Paul Moss <moss.paul@gmail.com>
2017-03-16 15:16:52 +0000

    log this at INFO

--- freelancefinder/users/middleware/xforwardedfor.py
+++ freelancefinder/users/middleware/xforwardedfor.py
@@ -12,12 +12,12 @@ class XForwardedForMiddleware(MiddlewareMixin):
     def __init__(self, get_response):
         """Init the middleware."""
         self.get_response = get_response
-        logger.debug("initializing x-forwarded-for middleware.")
+        logger.info("initializing x-forwarded-for middleware.")
 
     def process_request(self, request):
         """Process request, add header."""
-        logger.debug("X-Forwarded-For middleware: %s", request.META)
+        logger.info("X-Forwarded-For middleware: %s", request.META)
         if ('REMOTE_ADDR' not in request.META or len(request.META['REMOTE_ADDR']) == 0) and 'HTTP_X_FORWARDED_FOR' in request.META:
-            logger.debug("X-Forwarded-For middleware setting REMOTE ADDR to: %s", request.META['HTTP_X_FORWARDED_FOR'])
+            logger.info("X-Forwarded-For middleware setting REMOTE ADDR to: %s", request.META['HTTP_X_FORWARDED_FOR'])
             request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR'].split(",")[0].strip()
         return None
