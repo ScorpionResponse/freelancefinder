1891ac888cc8af988d98c814d70982ff0116d2c6
Paul Moss <moss.paul@gmail.com>
2017-03-16 14:54:31 +0000

    try using middlewaremixin

--- freelancefinder/users/middleware/xforwardedfor.py
+++ freelancefinder/users/middleware/xforwardedfor.py
@@ -1,10 +1,12 @@
 """X-Forwarded-For processing middleware."""
 import logging
 
+from django.utils.deprecation import MiddlewareMixin
+
 logger = logging.getLogger(__name__)
 
 
-class XForwardedForMiddleware(object):
+class XForwardedForMiddleware(MiddlewareMixin):
     """Middleware to add appropriate REMOTE_ADDR header."""
 
     def __init__(self, get_response):
@@ -12,19 +14,10 @@ class XForwardedForMiddleware(object):
         self.get_response = get_response
         logger.debug("initializing x-forwarded-for middleware.")
 
-    def __call__(self, request):
-        """Call the middleware."""
-
+    def process_request(self, request):
+        """Process request, add header."""
         logger.debug("X-Forwarded-For middleware: %s", request.META)
         if ('REMOTE_ADDR' not in request.META or len(request.META['REMOTE_ADDR']) == 0) and 'HTTP_X_FORWARDED_FOR' in request.META:
             logger.debug("X-Forwarded-For middleware setting REMOTE ADDR to: %s", request.META['HTTP_X_FORWARDED_FOR'])
             request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR'].split(",")[0].strip()
-
-        response = self.get_response(request)
-
-        return response
-
-    def process_request(self, request):
-        """Process."""
-        logger.debug("Processing request: %s", request.META)
-        return request
+        return None
