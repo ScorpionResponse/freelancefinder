54916f9bff032081e1d510ff30a0f0c40978305a
Paul Moss <moss.paul@gmail.com>
2017-03-16 13:43:45 +0000

    move middleware to users

--- freelancefinder/freelancefinder/settings.py
+++ freelancefinder/freelancefinder/settings.py
@@ -96,7 +96,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'freelancefinder.middleware.xforwardedfor.XForwardedForMiddleware',
+    'users.middleware.xforwardedfor.XForwardedForMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
