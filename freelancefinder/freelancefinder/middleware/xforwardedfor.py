d335126eb4255533cef3dcf3ab4beabd26dd7b3a
Paul Moss <moss.paul@gmail.com>
2017-03-16 12:38:52 +0000

    use new style middleware

--- freelancefinder/freelancefinder/middleware/xforwardedfor.py
+++ freelancefinder/freelancefinder/middleware/xforwardedfor.py
@@ -4,9 +4,16 @@
 class XForwardedForMiddleware(object):
     """Middleware to add appropriate REMOTE_ADDR header."""
 
-    def process_request(self, request):
-        """Insert appropriate REMOTE_ADDR header."""
+    def __init__(self, get_response):
+        """Init the middleware."""
+        self.get_response = get_response
+
+    def __call__(self, request):
+        """Call the middleware."""
 
         if 'REMOTE_ADDR' not in request.META and 'X_FORWARDED_FOR' in request.META:
             request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR'].split(",")[0].strip()
-        return None
+
+        response = self.get_response(request)
+
+        return response
