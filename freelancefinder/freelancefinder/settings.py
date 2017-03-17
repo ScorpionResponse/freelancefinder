8faa6abf9a9a17b485866b663b0760aad4eb0390
Paul Moss <moss.paul@gmail.com>
2017-03-17 21:48:48 +0000

    Move the middleware back to the freelancefinder app

--- freelancefinder/freelancefinder/settings.py
+++ freelancefinder/freelancefinder/settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'users.middleware.xforwardedfor.XForwardedForMiddleware',
+    'freelancefinder.middleware.xforwardedfor.XForwardedForMiddleware',
 ]
 
 if DEBUG:
