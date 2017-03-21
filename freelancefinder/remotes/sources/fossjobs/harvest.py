e0b9d98f70775fd0070ff80a0611a39057f985ba
Paul Moss <moss.paul@gmail.com>
2017-03-21 12:20:10 +0000

    try to harvest once an hour

--- freelancefinder/remotes/sources/fossjobs/harvest.py
+++ freelancefinder/remotes/sources/fossjobs/harvest.py
@@ -22,7 +22,7 @@ class Harvester(object):
 
     def harvest(self):
         """Gather some Posts from hackernews."""
-        today = datetime.date.today().strftime("%Y-%m-%d")
+        today = datetime.datetime.today().strftime("%Y-%m-%d-%H")
         if self.source.config.filter(config_key='processed_date-rss_feed', config_value=today).exists():
             return
         rss_feed = feedparser.parse('https://www.fossjobs.net/rss/all/')
@@ -31,6 +31,8 @@ class Harvester(object):
                 logger.debug('Alread processed this item %s, skipping the rest.', job_info.id)
                 break
             post = Post(url=job_info.link, source=self.source, title=job_info.title, description=bleach.clean(job_info.description, strip=True), unique=job_info.id, subarea='all', is_job_posting=True)
+            self.status_info['count-rss'] += 1
+            self.status_info['total'] += 1
             yield post
         logger.info("FossJobs harvester status: %s", dict(self.status_info))
         self.source.config.update_or_create(config_key='processed_date-rss_feed', defaults={'config_value': today})
