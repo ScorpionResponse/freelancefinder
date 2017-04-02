4f6c725fc10f67871b1b5c94eb52cf1c4a426ce1
Paul Moss <moss.paul@gmail.com>
2017-04-02 19:17:44 +0000

    add simple source test

--- freelancefinder/remotes/test_remotes.py
+++ freelancefinder/remotes/test_remotes.py
@@ -1,5 +1,7 @@
 """Tests for the remotes app."""
 
+from .models import Source
+
 
 def test_source_list(client):
     """Test source list page returns 200."""
@@ -9,7 +11,15 @@ def test_source_list(client):
 
 def test_harvester():
     """Test that getting harvester returns something."""
-    from .models import Source
     for source in Source.objects.all():
         harvester = source.harvester()
         assert harvester is not None
+
+
+def test_add_source():
+    """Add a source and test outcome."""
+    start_count = Source.objects.all().count()
+    new_source = Source.objects.create(code='new', name='New Source', url='http://test.example.com/')
+    assert 'New Source' in str(new_source)
+    assert new_source is not None
+    assert Source.objects.all().count() == 1 + start_count
