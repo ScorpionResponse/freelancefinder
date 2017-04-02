0ba59f9e0bc720de534d4cb9bad77c4a443342c2
Paul Moss <moss.paul@gmail.com>
2017-04-02 19:44:05 +0000

    add simple create tests

--- freelancefinder/jobs/test_jobs.py
+++ freelancefinder/jobs/test_jobs.py
@@ -31,6 +31,13 @@ class JobTests(TestCase):
         self.job = Job.objects.create(title='A New Job', description="Some Description")
         self.client = Client()
 
+    def test_create_job(self):
+        job = Job.objects.create(title='Another New Job', description="Some Other Description")
+        num_jobs = Job.objects.all().count()
+        assert num_jobs != 0
+        assert job is not None
+        assert 'New Job' in str(job)
+
     def test_get_jobs(self):
         num_jobs = Job.objects.all().count()
         assert num_jobs != 0
@@ -47,8 +54,15 @@ class FreelancerTests(TestCase):
     """Tests related to handling freelancers."""
 
     def setUp(self):
-        self.freelancer = Freelancer.objects.create(title='A New Job', description="Some Description")
+        self.freelancer = Freelancer.objects.create(title='A New Freelancer', description="Some Description")
         self.client = Client()
+
+    def test_create_freelancer(self):
+        freelancer = Freelancer.objects.create(title='Another New Freelancer', description="Some Other Description")
+        num_freelancers = Freelancer.objects.all().count()
+        assert num_freelancers != 0
+        assert freelancer is not None
+        assert 'New Freelancer' in str(freelancer)
 
     def test_get_freelancers(self):
         num_freelancers = Freelancer.objects.all().count()
@@ -71,6 +85,14 @@ class PostTests(TestCase):
         self.post = Post.objects.create(job=self.job, url='http://www.google.com/', source=self.source,
                                         title='A New Job', description="Some Description")
         self.client = Client()
+
+    def test_create_post(self):
+        post = Post.objects.create(job=self.job, url='http://test.example.com/', source=self.source,
+                                   title='A New Post', description="Some Silly Description")
+        num_posts = Post.objects.all().count()
+        assert num_posts != 0
+        assert post is not None
+        assert 'New Post' in str(post)
 
     def test_get_posts(self):
         num_jobs = Post.objects.all().count()
