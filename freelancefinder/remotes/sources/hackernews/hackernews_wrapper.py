9f3db8363d2ebf8f8708ff2dfaafabb7f7cc080b
Paul Moss <moss.paul@gmail.com>
2017-04-04 22:20:41 +0000

    redo hackernews harvester

---
+++ freelancefinder/remotes/sources/hackernews/hackernews_wrapper.py
@@ -0,0 +1,127 @@
+"""Wrapper for hackernews."""
+
+import datetime
+import logging
+
+import bleach
+from hackernews import HackerNews, InvalidItemID
+
+from django.utils import timezone
+
+from jobs.models import Post
+
+logger = logging.getLogger(__name__)
+
+
+class HackerHarvest(object):
+    """Wrapper client for hackernews harvester."""
+
+    def __init__(self, source):
+        """Initialize the harvester."""
+        self.source = source
+        self.client = HackerNews()
+
+    def job_stories(self):
+        """Gather job postings and turn them into posts."""
+        for story_id in self.client.job_stories(limit=100):
+            story = self.client.get_item(story_id)
+            post = self.parse_job_to_post(story, subarea='jobs')
+            post.is_job_posting = True
+            yield post
+
+    def hiring_jobs(self):
+        """Gather posts from the Who is Hiring? thread."""
+        post_id = self.source.config.filter(config_key='post_id-who_is_hiring').first().config_value
+        hn_item = self.client.get_item(post_id)
+        # r'\s*(?P<company>[^|]+?)\s*\|\s*(?P<title>[^|]+?)\s*\|\s*(?P<locations>[^|]+?)\s*(?:\|\s*(?P<attrs>.+))?$'
+        for comment_id in hn_item.kids:
+            try:
+                comment = self.client.get_item(comment_id)
+            except InvalidItemID as iiid:
+                logger.warning('Tried to get non-existent comment with ID: %s; ex: %s', comment_id, iiid)
+                continue
+            if comment.text is None:
+                logger.debug("Skipping blank comment: %s", comment)
+                continue
+            post = self.parse_job_to_post(comment, subarea='who_is_hiring')
+            post.is_job_posting = True
+            yield post
+
+    def who_wants_jobs(self):
+        """Gather posts from the Who wants to be hired? thread."""
+        post_id = self.source.config.filter(config_key='post_id-who_wants_to_be_hired').first().config_value
+        hn_item = self.client.get_item(post_id)
+        for comment_id in hn_item.kids:
+            try:
+                comment = self.client.get_item(comment_id)
+            except InvalidItemID as iiid:
+                logger.warning('Tried to get non-existent comment with ID: %s; ex: %s', comment_id, iiid)
+                continue
+            if comment.text is None:
+                logger.debug("Skipping blank comment: %s", comment)
+                continue
+            post = self.parse_job_to_post(comment, subarea='who_wants_to_be_hired', insert_author=True)
+            post.is_freelancer = True
+            yield post
+
+    def freelancer_jobs(self):
+        """Gather posts from the Freelancers thread."""
+        post_id = self.source.config.filter(config_key='post_id-freelancer').first().config_value
+        hn_item = self.client.get_item(post_id)
+        for comment_id in hn_item.kids:
+            try:
+                comment = self.client.get_item(comment_id)
+            except InvalidItemID as iiid:
+                logger.warning('Tried to get non-existent comment with ID: %s; ex: %s', comment_id, iiid)
+                continue
+            if comment.text is None:
+                logger.debug("Skipping blank comment: %s", comment)
+                continue
+            post = self.parse_job_to_post(comment, subarea='freelancer')
+            post.is_freelance = True
+            if 'SEEKING WORK' in post.title.upper():
+                post.is_freelancer = True
+            elif 'SEEKING FREELANCER' in post.title.upper():
+                post.is_job_posting = True
+            yield post
+
+    def check_who_is_hiring(self):
+        """Check for new who is hiring posts every month."""
+        month_year = datetime.date.today().strftime("%B %Y")
+        if self.source.config.filter(config_key='processed_date-last_month', config_value=month_year).exists():
+            return
+        who_is_hiring_user = self.client.get_user('whoishiring')
+        new_posts = [False, False, False]
+        for post_id in who_is_hiring_user.submitted[:7]:
+            hn_item = self.client.get_item(post_id)
+            if month_year in hn_item.title:
+                if 'Who is hiring?' in hn_item.title:
+                    new_posts[0] = post_id
+                if 'Freelancer? Seeking freelancer?' in hn_item.title:
+                    new_posts[1] = post_id
+                if 'Who wants to be hired?' in hn_item.title:
+                    new_posts[2] = post_id
+
+        if all(new_posts):
+            self.source.config.update_or_create(config_key='processed_date-last_month', defaults={'config_value': month_year})
+            self.source.config.update_or_create(config_key='post_id-who_is_hiring', defaults={'config_value': new_posts[0]})
+            self.source.config.update_or_create(config_key='post_id-freelancer', defaults={'config_value': new_posts[1]})
+            self.source.config.update_or_create(config_key='post_id-who_wants_to_be_hired', defaults={'config_value': new_posts[2]})
+
+    def parse_job_to_post(self, job_info, subarea, insert_author=False):
+        """Convert a comment or story to a Post."""
+        title = job_info.title
+        if title is None:
+            title = job_info.text.split('<')[0]
+        if insert_author:
+            title = job_info.by + ' - ' + title
+        title_cleaned = bleach.clean(title[:255], strip=True)
+        desc = ''
+        if job_info.text:
+            desc = job_info.text
+        url = job_info.url
+        if not url:
+            url = "https://news.ycombinator.com/item?id={}".format(job_info.item_id)
+        created = timezone.make_aware(job_info.submission_time)
+        post = Post(url=url, source=self.source, title=title_cleaned, description=desc, unique=job_info.item_id, created=created, subarea=subarea)
+        return post
--- freelancefinder/remotes/sources/hackernews/harvest.py
+++ freelancefinder/remotes/sources/hackernews/harvest.py
@@ -1,16 +1,12 @@
 """Harvest process for the HackerNews Source."""
 
-import datetime
 import itertools
 import logging
 from collections import defaultdict
 
-import bleach
-from hackernews import HackerNews, InvalidItemID
+from remotes.decorators import periodically
 
-from django.utils import timezone
-
-from jobs.models import Post
+from .hackernews_wrapper import HackerHarvest
 
 logger = logging.getLogger(__name__)
 
@@ -21,7 +17,7 @@ class Harvester(object):
     def __init__(self, source):
         """Init the hackernews harvester."""
         self.source = source
-        self.client = HackerNews()
+        self.hackernews = HackerHarvest(self.source)
         self.status_info = defaultdict(int)
 
     def harvest(self):
@@ -31,48 +27,22 @@ class Harvester(object):
             yield post
         logger.info("HackerNews harvester status: %s", dict(self.status_info))
 
+    @periodically(period='daily', check_name='last_processed-job_stories')
     def _process_job_stories(self):
         """Process the job_stories from hackernews."""
-        for story_id in self.client.job_stories(limit=100):
-            story = self.client.get_item(story_id)
-            if Post.objects.filter(source=self.source, unique=story.item_id).exists():
-                logger.info("HackerNews harvester got duplicate post id %s, assuming everything new is harvested.", story.item_id)
+        for post in self.hackernews.job_stories():
+            if post.exists():
+                logger.info("HackerNews harvester got duplicate post id %s, assuming everything new is harvested.", post)
                 break
-            else:
-                desc = ''
-                if story.text:
-                    desc = story.text
-                url = story.url
-                if not url:
-                    url = "https://news.ycombinator.com/item?id={}".format(story.item_id)
-                created = timezone.make_aware(story.submission_time)
-                post = Post(url=url, source=self.source, title=bleach.clean(story.title[:255], strip=True), description=desc, unique=story.item_id, created=created, subarea='jobs', is_job_posting=True)
-                self.status_info['count-job'] += 1
-                self.status_info['total'] += 1
-                yield post
 
+            self.status_info['count-job'] += 1
+            self.status_info['total'] += 1
+            yield post
+
+    @periodically(period='daily', check_name='last_processed-new_threads')
     def _check_for_new_hiring_threads(self):
         """Find new hiring posts from the whoishiring user."""
-        month_year = datetime.date.today().strftime("%B %Y")
-        if self.source.config.filter(config_key='processed_date-last_month', config_value=month_year).exists():
-            return
-        who_is_hiring_user = self.client.get_user('whoishiring')
-        new_posts = [False, False, False]
-        for post_id in who_is_hiring_user.submitted[:7]:
-            hn_item = self.client.get_item(post_id)
-            if month_year in hn_item.title:
-                if 'Who is hiring?' in hn_item.title:
-                    new_posts[0] = post_id
-                if 'Freelancer? Seeking freelancer?' in hn_item.title:
-                    new_posts[1] = post_id
-                if 'Who wants to be hired?' in hn_item.title:
-                    new_posts[2] = post_id
-
-        if all(new_posts):
-            self.source.config.update_or_create(config_key='processed_date-last_month', defaults={'config_value': month_year})
-            self.source.config.update_or_create(config_key='post_id-who_is_hiring', defaults={'config_value': new_posts[0]})
-            self.source.config.update_or_create(config_key='post_id-freelancer', defaults={'config_value': new_posts[1]})
-            self.source.config.update_or_create(config_key='post_id-who_wants_to_be_hired', defaults={'config_value': new_posts[2]})
+        self.hackernews.check_who_is_hiring()
 
     def _process_threads(self):
         """Process each hiring thread."""
@@ -82,97 +52,41 @@ class Harvester(object):
             self._process_freelancer()
         )
 
+    @periodically(period='daily', check_name='last_processed-who_is_hiring')
     def _process_who_is_hiring(self):
         """Process the Who is hiring? thread."""
-        today = datetime.date.today().strftime("%Y-%m-%d")
-        if self.source.config.filter(config_key='processed_date-who_is_hiring', config_value=today).exists():
-            return
         logger.info("Processing who is hiring post")
-        post_id = self.source.config.filter(config_key='post_id-who_is_hiring').first().config_value
-        hn_item = self.client.get_item(post_id)
-        # r'\s*(?P<company>[^|]+?)\s*\|\s*(?P<title>[^|]+?)\s*\|\s*(?P<locations>[^|]+?)\s*(?:\|\s*(?P<attrs>.+))?$'
-        for comment_id in hn_item.kids:
-            if Post.objects.filter(source=self.source, unique=comment_id).exists():
-                logger.debug('Already processed comment %s.', comment_id)
+        for post in self.hackernews.hiring_jobs():
+            if post.exists():
+                logger.debug('Already processed %s.', post)
                 continue
-            try:
-                comment = self.client.get_item(comment_id)
-            except InvalidItemID as iiid:
-                logger.warning('Tried to get non-existent comment with ID: %s; ex: %s', comment_id, iiid)
-                continue
-            if comment.text is None:
-                logger.debug("Skipping blank comment: %s", comment)
-                continue
-            url = "https://news.ycombinator.com/item?id={}".format(comment_id)
-            title = bleach.clean(comment.text.split('<')[0][:255], strip=True)
-            created = timezone.make_aware(comment.submission_time)
-            post = Post(url=url, source=self.source, title=title, description=comment.text, unique=comment_id, created=created, subarea='who_is_hiring', is_job_posting=True)
             self.status_info['count-who_is_hiring'] += 1
             self.status_info['total'] += 1
             yield post
-        self.source.config.update_or_create(config_key='processed_date-who_is_hiring', defaults={'config_value': today})
 
+    @periodically(period='daily', check_name='last_processed-who_wants_to_be_hired')
     def _process_who_wants_to_be_hired(self):
         """Process the Who wants to be hired? thread."""
-        today = datetime.date.today().strftime("%Y-%m-%d")
-        if self.source.config.filter(config_key='processed_date-who_wants_to_be_hired', config_value=today).exists():
-            return
         logger.info("Processing who wants to be hired post")
-        post_id = self.source.config.filter(config_key='post_id-who_wants_to_be_hired').first().config_value
-        hn_item = self.client.get_item(post_id)
-        for comment_id in hn_item.kids:
-            if Post.objects.filter(source=self.source, unique=comment_id).exists():
-                logger.debug('Already processed comment %s.', comment_id)
+        for post in self.hackernews.who_wants_jobs():
+            if post.exists():
+                logger.debug('Already processed %s.', post)
                 continue
-            try:
-                comment = self.client.get_item(comment_id)
-            except InvalidItemID as iiid:
-                logger.warning('Tried to get non-existent comment with ID: %s; ex: %s', comment_id, iiid)
-                continue
-            if comment.text is None:
-                logger.debug("Skipping blank comment: %s", comment)
-                continue
-            url = "https://news.ycombinator.com/item?id={}".format(comment_id)
-            title = bleach.clean(comment.by + ' - ' + comment.text.split('<')[0], strip=True)
-            created = timezone.make_aware(comment.submission_time)
-            post = Post(url=url, source=self.source, title=title[:255], description=comment.text, unique=comment_id, created=created, subarea='who_wants_to_be_hired', is_freelancer=True)
             self.status_info['count-who_wants_to_be_hired'] += 1
             self.status_info['total'] += 1
             yield post
-        self.source.config.update_or_create(config_key='processed_date-who_wants_to_be_hired', defaults={'config_value': today})
 
+    @periodically(period='daily', check_name='last_processed-freelancer')
     def _process_freelancer(self):
         """Process the Freelancer/Seeking freelancer? thread."""
-        today = datetime.date.today().strftime("%Y-%m-%d")
-        if self.source.config.filter(config_key='processed_date-freelancer', config_value=today).exists():
-            return
         logger.info("Processing freelancer post")
-        post_id = self.source.config.filter(config_key='post_id-freelancer').first().config_value
-        hn_item = self.client.get_item(post_id)
-        for comment_id in hn_item.kids:
-            if Post.objects.filter(source=self.source, unique=comment_id).exists():
-                logger.debug('Already processed comment %s.', comment_id)
+        for post in self.hackernews.freelancer_jobs():
+            if post.exists():
+                logger.debug('Already processed %s.', post)
                 continue
-            try:
-                comment = self.client.get_item(comment_id)
-            except InvalidItemID as iiid:
-                logger.warning('Tried to get non-existent comment with ID: %s; ex: %s', comment_id, iiid)
-                continue
-            if comment.text is None:
-                logger.debug("Skipping blank comment: %s", comment)
-                continue
-            url = "https://news.ycombinator.com/item?id={}".format(comment_id)
-            title = bleach.clean(comment.by + ' - ' + comment.text.split('<')[0], strip=True)
-            created = timezone.make_aware(comment.submission_time)
-            post = Post(url=url, source=self.source, title=title[:255], description=comment.text, unique=comment_id, created=created, subarea='freelancer', is_freelance=True)
-            if 'SEEKING WORK' in title.upper():
-                post.is_freelancer = True
-            elif 'SEEKING FREELANCER' in title.upper():
-                post.is_job_posting = True
             self.status_info['count-freelancer'] += 1
             self.status_info['total'] += 1
             yield post
-        self.source.config.update_or_create(config_key='processed_date-freelancer', defaults={'config_value': today})
 
     def status(self):
         """Return the current status of this harvester."""
