d555454cd3765639c18507552e7f008367b1cbd3
Paul Moss <moss.paul@gmail.com>
2017-04-05 00:14:13 +0000

    100% coverage for reddit, I think

--- freelancefinder/remotes/tests/test_reddit.py
+++ freelancefinder/remotes/tests/test_reddit.py
@@ -9,28 +9,35 @@ from ..sources.reddit.harvest import Harvester
 
 
 class PrawMock(object):
+    """Mock praw.Reddit object."""
 
     class Submission(object):
+        """Mock the submission object."""
 
         def __init__(self, fields):
-            for k, v in fields.items():
-                setattr(self, k, v)
+            """Just turn dict keys into attributes."""
+            for key, value in fields.items():
+                setattr(self, key, value)
 
     def __init__(self):
         pass
 
     def __call__(self, *args, **kwargs):
+        """Just return."""
         return self
 
     def Reddit(self, *args, **kwargs):
+        """Just return."""
         return self
 
     def subreddit(self, *args, **kwargs):
+        """Just return."""
         return self
 
     def new(self, limit):
+        """Return a bunch of random post like things."""
         fake = Faker()
-        for i in range(limit):
+        for times in range(limit):
             subm = {
                 'url': fake.url(),
                 'title': "{} {}".format(random.choice(['[HIRING]', '[FOR HIRE]', '']), fake.job()),
@@ -43,9 +50,32 @@ class PrawMock(object):
 
 
 def test_harvester(mocker):
+    """Test the reddit harvester."""
     mocker.patch('praw.Reddit', new_callable=PrawMock)
     source = Source.objects.get(code='reddit')
     harvester = Harvester(source)
     jobs = harvester.harvest()
     assert jobs is not None
     assert len(list(jobs)) >= 100
+
+
+def test_status_info(mocker):
+    """Test the reddit harvester."""
+    mocker.patch('praw.Reddit', new_callable=PrawMock)
+    source = Source.objects.get(code='reddit')
+    harvester = Harvester(source)
+    assert harvester.status()['total'] == 0
+    jobs = list(harvester.harvest())
+    assert harvester.status()['total'] > 0
+    assert harvester.status()['total'] == len(jobs)
+
+
+def test_post_exists_does_nothing(mocker):
+    """Test the reddit harvester stops on duplicates."""
+    mocker.patch('praw.Reddit', new_callable=PrawMock)
+    mocker.patch('jobs.models.Post.exists', side_effect=lambda: True)
+    source = Source.objects.get(code='reddit')
+    harvester = Harvester(source)
+    jobs = list(harvester.harvest())
+    assert harvester.status()['total'] == 0
+    assert harvester.status()['total'] == len(jobs)
