e8c0b0234550ac415565e566df32a24c9d3da33d
Paul Moss <moss.paul@gmail.com>
2017-04-06 11:13:17 +0000

    temporarily switch to SITE=2

--- freelancefinder/remotes/migrations/0004_auto_20170222_1422.py
+++ freelancefinder/remotes/migrations/0004_auto_20170222_1422.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 NEW_SOURCE_CONFIG = {
-    'reddit': {'subreddits': ['freelance_forhire', 'forhire', 'python', 'django']},
+    'reddit': {'subreddits': ['freelance_forhire', 'forhire', 'python', 'django', 'nsfwforhire', 'sysadminjobs']},
 }
 
 
