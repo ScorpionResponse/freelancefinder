a4908bfb59c34e44e7b910eee1fad85e84fdbcb9
Paul Moss <moss.paul@gmail.com>
2017-04-16 14:12:47 +0000

    adjust user tests for new fixtures

--- freelancefinder/conftest.py
+++ freelancefinder/conftest.py
@@ -8,48 +8,68 @@ from django.contrib.auth.models import Group
 from jobs.tests.factories import JobFactory, PostFactory, FreelancerFactory, SourceFactory, TagFactory
 
 
-@pytest.fixture(scope="function")
-def debug_group_client(client, django_user_model):
-    """Get a client in the Debuggers group."""
+@pytest.fixture
+def debug_user(django_user_model):
+    """Get a user in the Debuggers group."""
     new_user = django_user_model.objects.create_user(username='debug_user', email='debug@example.com')
     group, created = Group.objects.get_or_create(name='Debuggers')
     new_user.groups.add(group)
     new_user.save()
+    return new_user
 
-    client.force_login(new_user)
+
+@pytest.fixture
+def debug_group_client(client, debug_user):
+    """Get a client in the Debuggers group."""
+    client.force_login(debug_user)
     return client
 
 
-@pytest.fixture(scope="function")
-def admin_group_client(client, django_user_model):
-    """Get a client in the Administrators group."""
+@pytest.fixture
+def admin_user(django_user_model):
+    """Get a user in the Administrators group."""
     new_user = django_user_model.objects.create_user(username='admin_user', email='admin@example.com')
     group, created = Group.objects.get_or_create(name='Administrators')
     new_user.groups.add(group)
     new_user.save()
+    return new_user
 
-    client.force_login(new_user)
+
+@pytest.fixture
+def admin_group_client(client, admin_user):
+    """Get a client in the Administrators group."""
+    client.force_login(admin_user)
     return client
 
 
-@pytest.fixture(scope="function")
-def paid_group_client(client, django_user_model):
-    """Get a client in the Paid group."""
+@pytest.fixture
+def paid_user(django_user_model):
+    """Get a user in the Paid group."""
     new_user = django_user_model.objects.create_user(username='paid_user', email='paid@example.com')
     group, created = Group.objects.get_or_create(name='Paid')
     new_user.groups.add(group)
     new_user.save()
+    return new_user
 
-    client.force_login(new_user)
+
+@pytest.fixture
+def paid_group_client(client, paid_user):
+    """Get a client in the Paid group."""
+    client.force_login(paid_user)
     return client
 
 
-@pytest.fixture(scope="function")
-def authed_client(client, django_user_model):
-    """Get a client that is logged in."""
+@pytest.fixture
+def authed_user(django_user_model):
+    """Get a logged in user."""
     new_user = django_user_model.objects.create_user(username='joe_user', email='joe@example.com')
+    return new_user
 
-    client.force_login(new_user)
+
+@pytest.fixture
+def authed_client(client, authed_user):
+    """Get a client that is logged in."""
+    client.force_login(authed_user)
     return client
 
 
--- freelancefinder/users/tests/test_users.py
+++ freelancefinder/users/tests/test_users.py
@@ -1,46 +1,27 @@
 """Tests related to users and accounts."""
 
-from django.contrib.auth.models import Group
 
-
-def test_login_changes(client, admin_user):
-    """Test that the page changes on login."""
-    client.force_login(admin_user)
-    response = client.get('/')
+def test_login_changes(authed_client):
+    """Test that the page changes when logged in."""
+    response = authed_client.get('/')
     assert 'AUTH:YES' in response.content.decode('utf-8')
 
 
-def test_no_group(client, django_user_model):
+def test_no_group(authed_client):
     """Test that page changes based on Group affiliation - no group."""
-    new_user = django_user_model.objects.create_user(username='joe', email='joe@example.com')
-    client.force_login(new_user)
-    response = client.get('/')
+    response = authed_client.get('/')
     assert '>Jobs<' in response.content.decode('utf-8')
 
 
-def test_admin_group(client, django_user_model):
+def test_admin_group(admin_group_client):
     """Test that page changes based on Group affiliation - Administrators group."""
-    new_user = django_user_model.objects.create_user(username='admin_user', email='admin@example.com')
-    group, created = Group.objects.get_or_create(name='Administrators')
-    new_user.groups.add(group)
-    new_user.save()
-    assert not created
-
-    client.force_login(new_user)
-    response = client.get('/')
+    response = admin_group_client.get('/')
     assert '>Posts<' in response.content.decode('utf-8')
 
 
-def test_debug_group(client, django_user_model):
+def test_debug_group(debug_group_client):
     """Test that page changes based on Group affiliation - Debuggers group."""
-    new_user = django_user_model.objects.create_user(username='debug_user', email='debug@example.com')
-    group, created = Group.objects.get_or_create(name='Debuggers')
-    new_user.groups.add(group)
-    new_user.save()
-    assert not created
-
-    client.force_login(new_user)
-    response = client.get('/')
+    response = debug_group_client.get('/')
     assert '>Sources<' in response.content.decode('utf-8')
 
 
