a4487112e06e2291fa415b2b6562086973bc5684
Paul Moss <moss.paul@gmail.com>
2017-05-02 14:22:06 +0000

    detect html

--- freelancefinder/freelancefinder/templatetags/markdown_filter.py
+++ freelancefinder/freelancefinder/templatetags/markdown_filter.py
@@ -8,13 +8,16 @@ import markdown as markdown_
 
 register = template.Library()
 
-# ADDITIONAL_TAGS = ['p', 'br', 'pre']
-ADDITIONAL_TAGS = ['p', 'br']
+ADDITIONAL_TAGS = ['p', 'br', 'pre']
 
 
 @register.filter(name='markdown')
 def markdown_filter(value):
     """Convert markdown value to html."""
-    rendered_html = markdown_.markdown(value)
+    if value.count('<') > 5:
+        # Assume this is HTML and not markdown
+        rendered_html = value
+    else:
+        rendered_html = markdown_.markdown(value)
     bleached_html = bleach.clean(rendered_html, tags=bleach.ALLOWED_TAGS + ADDITIONAL_TAGS, strip=True)
     return mark_safe(bleached_html)
