fc7df6d29e9c72cfb7b854d087dc71427a0874e0
Paul Moss <moss.paul@gmail.com>
2017-05-09 22:37:23 +0000

    Try to start pulling together source history info

--- freelancefinder/remotes/views.py
+++ freelancefinder/remotes/views.py
@@ -1,6 +1,8 @@
 """Views for remotes app."""
 from braces.views import GroupRequiredMixin
 
+from django.db.models.aggregates import Count
+from django.db.models.functions.datetime import TruncDate
 from django.views.generic import ListView
 
 from .models import Source
@@ -12,3 +14,14 @@ class SourceListView(GroupRequiredMixin, ListView):
     model = Source
     group_required = u'Debuggers'
     template_name = "remotes/source_list.html"
+
+    def _get_harvest_history(self):
+        """Get a history of harvests executed."""
+        history = Source.objects.all().values('code', harvest_date=TruncDate('posts__modified')).annotate(post_count=Count('posts')).order_by('-harvest_date', 'name')
+        return history
+
+    def get_context_data(self, **kwargs):
+        """Add history to context info."""
+        context = super(SourceListView, self).get_context_data(**kwargs)
+        context['harvest_history'] = self._get_harvest_history()
+        return context
