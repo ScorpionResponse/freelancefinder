33bbc9f3ff65a8495b039ebb9e4007509103f873
Paul Moss <moss.paul@gmail.com>
2017-05-21 16:51:34 +0000

    add submit button

---
+++ freelancefinder/freelancefinder/forms.py
@@ -0,0 +1,15 @@
+"""Forms for the freelancefinder app."""
+
+from contact_form.forms import ContactForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+
+class CrispyContactForm(ContactForm):
+    """Contact form with a crispy form helper."""
+
+    def __init__(self, *args, **kwargs):
+        """Add crispy helpers."""
+        super(CrispyContactForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Send'))
--- freelancefinder/freelancefinder/urls.py
+++ freelancefinder/freelancefinder/urls.py
@@ -8,6 +8,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+from contact_form.views import ContactFormView
+
+from .forms import CrispyContactForm
 from .views import IndexPageView
 
 admin.autodiscover()
@@ -18,7 +21,8 @@ urlpatterns = [
     url(r'^remotes/', include('remotes.urls')),
     url(r'^users/', include('users.urls')),
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^contact/', include('contact_form.urls')),
+    url(r'^contact/$', ContactFormView.as_view(form_class=CrispyContactForm), name='contact_form'),
+    url(r'^contact/sent/$', TemplateView.as_view(template_name='contact_form/contact_form_sent.html'), name='contact_form_sent'),
     url(r'^select2/', include('django_select2.urls')),
     url(r'^freelance_admin/', admin.site.urls),
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'), name='robots.txt'),
